4ce965cca24eb4db4f5099bf5f2d367878261090
paveltitenkov <paveltitenkov@coherentsolutions.com>
2019-06-26 22:50:58 +0300

    versioning logic

--- ggvc.go
+++ ggvc.go
@@ -2,37 +2,114 @@ package main
 
 import (
   "fmt"
+  "strings"
   "log"
   "os"
+  "os/exec"
   "github.com/urfave/cli"
 )
 
 var app = cli.NewApp()
 
-func info() {
+func Info() {
 	app.Name = "Gradle git version calculator"
 	app.Usage = "Script for generating semantic version based on the version in gradle.properties and git branch"
-	app.Author = "Pavel Titenkov" 
 	app.Version = "0.0.1"
 }
 
-func commands() {
+func Commands() {
   app.Commands = []cli.Command{
     {
       Name:    "version",
-      Aliases: []string{"v", "-v", "--v", "--version"},
+      Aliases: []string{"v", "--version"},
       Usage:   "Calculate semantic version",
       Action: func(c *cli.Context) {
-        // TODO: implementation should come here..
-        fmt.Println("1.0.0")
+        version := CalculateSemanticVersion()
+        fmt.Printf("%s", version)
       },
     },
   }
 }
 
+// Calculates the semantic version depending on the gradle version (taken from the gradle.properties) and git branch
+//
+// Branch         gradle.version    semantic version
+// develop        1.0.0-SNAPSHOT    1.0.0-SNAPSHOT
+// some-branch    1.0.0-SNAPSHOT    1.0.0-some-branch-SNAPSHOT
+// feature/x      1.0.0-SNAPSHOT    1.0.0-feature-x-SNAPSHOT
+// defect/x       1.0.0-SNAPSHOT    1.0.0-feature-x-SNAPSHOT
+// release/1.x    1.0.0             1.0.0-rc{BUILD}
+// hotfix/1.x     1.0.0             1.0.0-rc{BUILD}
+// master         1.0.0             1.0.0
+func CalculateSemanticVersion() (string) {
+  var branch, version, versionRoot, versionExt, semanticVersion string
+
+  branch, _ = ResolveGitBranch()
+  version, _ = ResolveGradleVersion()
+
+  // convert brenches like feature/xyz to feature-xyz
+  branch = strings.Replace(branch, "/", "-", -1)
+
+  if strings.Contains(version, "-") {
+    versionRoot = strings.TrimSpace(strings.Split(version, "-")[0])
+    versionExt = strings.TrimSpace(strings.Split(version, "-")[1])
+  } else {
+    versionRoot = version
+    versionExt = ""
+  }
+
+  // Calculate semantic version
+  if branch == "develop" {
+    semanticVersion = versionRoot
+  } else if strings.HasPrefix(branch, "release/") || strings.HasPrefix(branch, "hotfix/") {
+    buildNumber := GetEnvVariable("BUILD_NUMBER", "0")
+    semanticVersion = fmt.Sprintf("%v-rc%v", versionRoot, buildNumber)
+  } else {
+    semanticVersion = fmt.Sprintf("%v-%v", versionRoot, branch)
+  }
+
+  if len(versionExt) > 0 {
+    semanticVersion = fmt.Sprintf("%v-%v", semanticVersion, versionExt)
+  }
+
+  return strings.TrimSpace(semanticVersion)
+}
+
+func ResolveGradleVersion() (string, error) {
+  out, err := exec.Command("bash", "-c", "cat gradle.properties | grep version | cut -d'=' -f2").Output()
+  return strings.TrimSpace(string(out[:])), err
+}
+
+func ResolveGitBranch() (string, error) {
+  // Determine the git branch from env if running on CI, otherwise from git
+  if IsEnvVariableDefined("BUILD_NUMBER") { // magic jenkins variable
+    if IsEnvVariableDefined("CHANGE_ID") { // Are we building a PR?
+      return GetEnvVariable("CHANGE_BRANCH", "unknown"), nil
+    } else { // Not a PR
+      return GetEnvVariable("BRANCH_NAME", "unknown"), nil
+    }
+  }
+  // Not a CI build
+  out, err := exec.Command("bash", "-c", "git rev-parse --abbrev-ref HEAD 2> /dev/null  || echo 'unknown'").Output()
+  return strings.TrimSpace(string(out[:])), err
+}
+
+func IsEnvVariableDefined(key string) (bool) {
+  _, exists := os.LookupEnv(key)
+  return exists
+}
+
+func GetEnvVariable(key, fallback string) (string) {
+  value, exists := os.LookupEnv(key)
+  if !exists {
+    value = fallback
+  }
+  return value
+}
+
 func main() {
-  info()
-  commands()
+  Info()
+  Commands()
 
   err := app.Run(os.Args)
   if err != nil {
--- ggvc.rb
+++ ggvc.rb
@@ -6,7 +6,8 @@ class Ggvc < Formula
   homepage "https://github.com/titenkov/gradle-git-version-calculator"
   url "https://github.com/titenkov/gradle-git-version-calculator/archive/v0.0.1.tar.gz"
   sha256 "8f01873a98531832905fac4d47e351fd4e13a9c47994fb2f2c7ea7cecb7b1ad8"
-  # depends_on "cmake" => :build
+  
+  depends_on "git"
 
   def install
     bin.install "ggvc"
